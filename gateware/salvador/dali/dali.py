e7d5afd8c2627bf979ec6e85f8fa61af181126be
dragonmux <git@dragonmux.network>
2021-09-19 00:43:51 +0100

    Plummed the new decoder into the DALI submodule tree

--- gateware/salvador/dali/dali.py
+++ gateware/salvador/dali/dali.py
@@ -1,6 +1,10 @@
 from nmigen import *
 from nmigen.build import Resource
+from .types import DeviceType
 from .serial import Serial
+from .decoder import CommandDecoder
+
+__all__ = ('DALI',)
 
 class DALI(Elaboratable):
 	def __init__(self, *, interface : Resource):
@@ -9,10 +13,17 @@ class DALI(Elaboratable):
 	def elaborate(self, platform):
 		m = Module()
 		m.submodules.serial = serial = Serial()
+		m.submodules.decoder = decoder = CommandDecoder(deviceType = DeviceType.led)
 		interface = self._interface
+
+		address = Signal(8)
+		command = Signal(8)
 
 		m.d.comb += [
 			serial.rx.eq(interface.rx.i),
 			interface.tx.o.eq(serial.tx),
+			address.eq(serial.dataOut[8:16]),
+			command.eq(serial.dataOut[0:8]),
+			decoder.commandByte.eq(command),
 		]
 		return m
