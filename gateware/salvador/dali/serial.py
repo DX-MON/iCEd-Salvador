c58f9fb5a0d6928a77fc68d6544ef7be46e1f99c
dragonmux <git@dragonmux.network>
2021-09-18 22:51:16 +0100

    Renamed all the receive related signals in the serial component for DALI to deconflict with transmit

--- gateware/salvador/dali/serial.py
+++ gateware/salvador/dali/serial.py
@@ -15,32 +15,31 @@ class Serial(Elaboratable):
 		m = Module()
 		timerCount = int(platform.default_clk_frequency) // self._bitRate
 		timerWidth = range(timerCount)
-		timer = Signal(timerWidth)
-		timerEnabled = Signal()
+		rxTimer = Signal(timerWidth)
+		rxTimerEnabled = Signal()
 
-		with m.If(timerEnabled):
-			with m.If(timer == (timerCount - 1)):
-				m.d.sync += timer.eq(0)
+		with m.If(rxTimerEnabled):
+			with m.If(rxTimer == (timerCount - 1)):
+				m.d.sync += rxTimer.eq(0)
 			with m.Else():
-				m.d.sync += timer.eq(timer + 1)
+				m.d.sync += rxTimer.eq(rxTimer + 1)
 		with m.Else():
-			m.d.sync += timer.eq(0)
+			m.d.sync += rxTimer.eq(0)
 
 		m.submodules.encoder = encoder = ManchesterEncoder()
 		m.submodules.decoder = decoder = ManchesterDecoder()
 
-		step = Signal()
-		cycle = Signal()
+		rxStep = Signal()
+		rxCycle = Signal()
 
 		m.d.comb += [
-			step.eq((timer == 0) & timerEnabled),
+			rxStep.eq((rxTimer == 0) & rxTimerEnabled),
 			self.dataAvailable.eq(0),
 
-			encoder.step.eq(step),
-			self.tx.eq(encoder.dataOut),
-
-			decoder.step.eq(step),
+			decoder.step.eq(rxStep),
 			decoder.dataIn.eq(self.rx),
+
+			self.tx.eq(encoder.dataOut),
 		]
 
 		rxDelayed = Signal()
@@ -54,36 +53,36 @@ class Serial(Elaboratable):
 		dataRXStopCount = Signal(range(2))
 		dataRXError = Signal()
 
-		with m.FSM(name = "rx-fsm"):
-			with m.State("IDLE"):
+		with m.FSM(name = 'rx-fsm'):
+			with m.State('IDLE'):
 				with m.If(startStrobe):
 					m.d.sync += [
-						timerEnabled.eq(1),
-						cycle.eq(0),
+						rxTimerEnabled.eq(1),
+						rxCycle.eq(0),
 					]
-					m.next = "START"
+					m.next = 'START'
 				with m.Else():
-					m.d.sync += timerEnabled.eq(0)
-			with m.State("START"):
-				with m.If(step):
-					m.d.sync += cycle.eq(cycle ^ 1)
-				with m.If(step & cycle):
-					m.next = "START-CHECK"
-			with m.State("START-CHECK"):
+					m.d.sync += rxTimerEnabled.eq(0)
+			with m.State('START'):
+				with m.If(rxStep):
+					m.d.sync += rxCycle.eq(rxCycle ^ 1)
+				with m.If(rxStep & rxCycle):
+					m.next = 'START-CHECK'
+			with m.State('START-CHECK'):
 				with m.If(decoder.valid):
 					m.d.sync += [
 						dataValid.eq(1),
 						dataRXCount.eq(0),
 					]
-					m.next = "SHIFT"
+					m.next = 'SHIFT'
 				with m.Else():
-					m.next = "IDLE"
-			with m.State("SHIFT"):
-				with m.If(step):
-					m.d.sync += cycle.eq(cycle ^ 1)
-				with m.If(step & cycle):
-					m.next = "SHIFT-CHECK"
-			with m.State("SHIFT-CHECK"):
+					m.next = 'IDLE'
+			with m.State('SHIFT'):
+				with m.If(rxStep):
+					m.d.sync += rxCycle.eq(rxCycle ^ 1)
+				with m.If(rxStep & rxCycle):
+					m.next = 'SHIFT-CHECK'
+			with m.State('SHIFT-CHECK'):
 				m.d.sync += [
 					dataRX.eq(Cat(decoder.dataOut , dataRX[:-1])),
 					dataRXCount.eq(dataRXCount + 1),
@@ -95,21 +94,19 @@ class Serial(Elaboratable):
 						decoder.bypass.eq(1),
 						dataRXStopCount.eq(0),
 					]
-					m.next = "STOP"
+					m.next = 'STOP'
 				with m.Else():
-					m.next = "SHIFT"
-			with m.State("STOP"):
-				with m.If(step):
-					m.d.sync += cycle.eq(cycle ^ 1)
-				with m.If(step & cycle):
+					m.next = 'SHIFT'
+			with m.State('STOP'):
+				with m.If(rxStep):
+					m.d.sync += rxCycle.eq(rxCycle ^ 1)
+				with m.If(rxStep & rxCycle):
 					m.d.sync += dataRXStopCount.eq(dataRXStopCount + 1)
 					with m.If(~decoder.valid):
 						m.d.sync == dataValid.eq(0)
 					with m.If(dataRXStopCount == 1):
-						with m.If(~dataRXError):
-							m.d.sync += self.dataOut.eq(dataRX)
 						m.d.sync += self.error.eq(dataRXError)
 						m.d.comb += self.dataAvailable.eq(1)
-						m.next = "IDLE"
+						m.next = 'IDLE'
 		m.d.comb += dataRXError.eq(dataValid & (~decoder.valid))
 		return m
