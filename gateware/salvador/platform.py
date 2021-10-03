67979ffd902d76f3eecdd1b76379830daaf7bc4f
dragonmux <git@dragonmux.network>
2021-10-03 01:13:46 +0100

    Added a SPI resource for the FRAM

--- gateware/salvador/platform.py
+++ gateware/salvador/platform.py
@@ -1,5 +1,6 @@
 from nmigen.build import Resource, Pins, Attrs, Clock
 from nmigen.vendor.lattice_ice40 import *
+from nmigen_boards.resources.interface import SPIResource
 from .resources import *
 
 __all__ = ('SalvadorPlatform',)
@@ -10,14 +11,18 @@ class SalvadorPlatform(LatticeICE40Platform):
 	default_clk = 'clk16MHz'
 
 	resources = [
-		Resource('clk16MHz', 0, Pins('35', dir='i'), Clock(16e6),
-			Attrs(GLOBAL=True, IO_STANDARD='SB_LVCMOS')
+		Resource('clk16MHz', 0, Pins('35', dir = 'i'), Clock(16e6),
+			Attrs(GLOBAL = True, IO_STANDARD = 'SB_LVCMOS')
 		),
 
 		DALIResource(0,
 			rx = '9', tx = '10',
-			attrs = Attrs(IO_STANDARD='SB_LVCMOS')
-		)
+			attrs = Attrs(IO_STANDARD = 'SB_LVCMOS')
+		),
+
+		SPIResource('fram', 0, cs_n = '36', clk = '37', copi = '38', cipo = '42',
+			role = 'controller',
+			attrs = Attrs(IO_STANDARD = 'SB_LVCMOS')),
 	]
 
 	connectors = []
