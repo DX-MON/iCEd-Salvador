efc5767327f90241c9846dcf9323e3c1871e28d6
dragonmux <git@dragonmux.network>
2021-10-24 01:52:54 +0100

    Added a action for generating the sim RTL for sim.dali.dali.readStartup

--- gateware/salvador/__init__.py
+++ gateware/salvador/__init__.py
@@ -9,6 +9,7 @@ def cli():
 		description = 'OPLSniffer')
 	actions = parser.add_subparsers(dest = 'action', required = True)
 	actions.add_parser('build', help = 'build a bitstream from the design')
+	actions.add_parser('prep-sim', help = 'prepare cxxrtl for the C++ based sims')
 
 	register_cli(parser = parser)
 	args = parser.parse_args()
@@ -16,9 +17,17 @@ def cli():
 	if args.action == 'arachne-sim':
 		from arachne.core.sim import run_sims
 		run_sims(pkg = 'salvador/sim', result_dir = 'build')
-		exit(0)
+		return 0
+	elif args.action == 'prep-sim':
+		from nmigen.back.cxxrtl import convert
+		from .sim.dali.dali import DALI, interface, DeviceType, Platform
+		dut = DALI(interface = interface, deviceType = DeviceType.led, persistResource = ('fram', 0))
+		with open('salvador/sim/dali/dali.hxx', 'wb') as file:
+			file.write(convert(dut, platform = Platform(clk_freq = 1e6)).encode())
+		return 0
 
 	platform = SalvadorPlatform()
 	salvador = Salvador()
 	if args.action == 'build':
 		platform.build(salvador, name = 'iCEdSalvador')
+	return 0
